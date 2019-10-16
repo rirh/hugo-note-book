1afce4ac83d6794a79fbbfa5429fb772eea2ad46
TigerZH <only_tigerhu@163.com>
2019-10-16 18:12:21 +0800

    await

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const spawn = require('child_process').spawn;
 
 const asyncGit = async () => {
+    return new Promise((rsolve, reject) => {
+        const git = require('simple-git')();
+        git.add('./*')
+            .commit("await")
+            .push('origin', 'master', () => {
+                console.log('done')
+            });
+    })
 
 };
-require('simple-git')()
-     .init()
-     .add('./*')
-     .commit("await")
-     .push('origin', 'master');
+asyncGit();
