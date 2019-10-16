93d0757ba5337423167d4f3c5fc0cca1e38c33a1
TigerZH <only_tigerhu@163.com>
2019-10-16 18:16:49 +0800

    await

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const spawn = require('child_process').spawn;
+const git = require('simple-git')();
+const CFonts = require('cfonts');
 
-const asyncGit = async () => {
-    return new Promise((rsolve, reject) => {
-        const git = require('simple-git')();
-        git.add('./*', () => {
-            console.log('add done')
-        })
-            .commit("await", () => {
-                console.log('commit done')
+const clg = (str) => {
+    CFonts.say(str, {
+        font: 'block', // define the font face
+        align: 'left', // define text alignment
+        colors: ['#fff', '#ff8344'], // define all colors
+        background: 'transparent', // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1, // define letter spacing
+        lineHeight: 1, // define the line height
+        space: true, // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0', // define how many character can be on one line
+    });
+}
+git.add('./*', () => {
+    console.log('add done')
+    clg('add done!')
+})
+    .commit("await", () => {
+        console.log('commit done')
+        clg('commit done!')
 
-            })
-            .push('origin', 'master', () => {
-                console.log('push done')
 
-            });
     })
+    .push('origin', 'master', () => {
+        clg('push done!')
 
-};
-asyncGit();
+        console.log('push done')
+
+    });
