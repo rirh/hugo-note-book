ae201ae524a1b63663596dba031a66ca391e6a3e
TigerZH <only_tigerhu@163.com>
2019-11-13 23:40:21 +0800

    await

---
+++ _book/roll1/coin.md
@@ -0,0 +1,11 @@
+# 加密货币
+
+**密码货币**（英文：Cryptocurrency，又译**密码学货币**、**加密货币**）是一种使用[密码学](https://zh.wikipedia.org/wiki/密碼學)原理来确保交易安全及控制交易单位创造的[交易介质](https://zh.wikipedia.org/wiki/交易媒介)。
+
+跟平常使用的纸币需要防伪设计一样，加密货币的防伪是利用数字货币和虚拟货币使用密码学及数字散列而成并与智能合约的绑定之下的新型通证。[比特币](https://zh.wikipedia.org/wiki/比特币)在2009年成为第一个[去中心化](https://zh.wikipedia.org/wiki/去中心化)的加密货币，这之后加密货币一词多指此类设计。自此之后数种类似的加密货币被创造，它们通常被称作altcoins。加密货币基于去中心化的共识机制，与依赖中心化监管体系的银行金融系统相对。
+
+去中心化的性质源自于使用[分布式账本](https://zh.wikipedia.org/wiki/分散式賬本)的[区块链](https://zh.wikipedia.org/wiki/区块链)（Blockchain）技术。
+
+
+
+![image-20191113230856649](/Users/tigerzh/Library/Application Support/typora-user-images/image-20191113230856649.png)
--- tou.js
+++ tou.js
@@ -1,56 +1,84 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
-const url = 'https://v2.jinrishici.com/one.json?client=browser-sdk/1.2&X-User-Token=Vjss1%2FliI2%2FXQ%2BtevC04GB2AvJ4IPJWG';
-var file = path.join(__dirname, 'test.json');
-const data = require('./test.json');
-// console.log(JSON.stringify(Array.from(new Set(data))));
-// fs.writeFile(file,JSON.stringify(Array.from(new Set(data))),function(){});
+const url = 'https://v2.jinrishici.com/one.json?client=mini-progrram-sdk/1.0';
 
-// 发送Get请求
-// 第一个参数:请求的完整URL,包括参数
-// 第二个参数:请求结果回调函数,会传入3个参数,第一个错误,第二个响应对象,第三个请求数据
-var content = [];
-function axios() {
-    return new Promise((reslove, reject) => {
-
-        request(url, function (err, res, data) {
-            if (err) reject(err);
-            reslove(data);
-        });
-
-    })
-};
-async function do_tou() {
-    var i = 0;
-    const read = () => {
-        fs.readFile(file, async function (err, res) {
-            if (err) reject(err);
-            var shiju = res.toString();
-            shiju = JSON.parse(shiju);
-            var total = shiju.length;
-            const data = await axios();
-            const con = JSON.parse(data);
-            const do_con = con.data.content;
-            shiju.push(do_con);
-            console.log(do_con);
-            console.log(total);
-            fs.writeFile(file, JSON.stringify(shiju), function (err) {
-                if (err) {
-                    return console.log(err);
+const AR = {
+    content: [],
+    file: path.join(__dirname, 'test.json'),
+    axios: function () {
+        return new Promise((reslove, reject) => {
+            request({
+                method: 'GET',
+                url,
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+                    'content-type': 'application/json;charset=UTF-8;q=0.8',
+                    'date': new Date(),
+                    // 'cookie': 'X-User-Token=Vjss1/liI2/XQ+tevC04GB2AvJ4IPJWG',
+                    // 'accept-encoding':'gzip, deflate, br',
+                    'accept-language':'zh-CN,zh;q=0.9,en;q=0.8,de;q=0.7',
+                    'cache-control':'max-age=0',
+                    'sec-fetch-mode':'navigate',
+                    'X-User-Token':'Vjss1/liI2/XQ+tevC04GB2AvJ4IPJWG'
+                    // ':authority': 'v2.jinrishici.com',
+                    // ':path': '/one.json?client=browser-sdk/1.2&X-User-Token=Vjss1%2FliI2%2FXQ%2BtevC04GB2AvJ4IPJWG',
+                    // ':method': 'GET',
+                    // ':scheme': 'https'
                 }
-                i++;
-                setTimeout(() => {
-                    if (i < 1000) read();
-                }, 400)
+
+            }, function (err, res, data) {
+                if (err) reject(err);
+                // console.log(data);
+                reslove(data);
+
             });
 
         })
+    },
+    do_tou: async function () {
+        var i = 0;
+        const read = () => {
+            fs.readFile(this.file, async (err, res) => {
+                if (err) reject(err);
+                var shiju = res.toString();
+                shiju = JSON.parse(shiju);
+                var total = shiju.length;
+                const data = await this.axios();
+                const con = JSON.parse(data);
+                const do_con = con.data.content;
+                const is_alive = shiju.some(e => e === con);
+                console.log(is_alive);
+                
+                if (!is_alive) {
+                    shiju.push(do_con);
+                    console.log(con);
+                    console.log(do_con);
+                    console.log(total);
+                };
+
+
+                fs.writeFile(this.file, JSON.stringify(shiju), function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    i++;
+                    setTimeout(() => {
+                        if (i < 1000) read();
+                    }, 1000)
+                });
+
+            })
+        }
+        read();
+    },
+    uniq: function () {
+        const data = require('./test.json');
+        console.log(Array.from(new Set(data)).length);
+        fs.writeFile(this.file, JSON.stringify(Array.from(new Set(data))), function () { });
     }
-    read();
 }
-// do_tou()
-
-
+AR.uniq();
+// AR.do_tou()
 
 
