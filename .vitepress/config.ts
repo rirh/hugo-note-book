e7de97688b0b3becf2f1474aabaa3c285cb862e4
TigerZH <only_tigerhu@163.com>
2022-03-23 16:42:32 +0800

    chrom: 更新配置

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -160,8 +160,12 @@ export const sidebar = {
     {
       text: '📚 使用指南',
       items: [
+        { text: 'git 指南', link: '/note/git' },
+        { text: 'liunx 指南', link: '/note/liunx' },
+        { text: 'Nginx 指南', link: '/note/nginx' },
+        { text: 'Docker 指南', link: '/note/Docker指南' },
+        { text: 'uWSGI 指南', link: '/note/uWSGI指南' },
         { text: 'Https配置指南', link: '/note/cert' },
-        { text: 'Nginx配置指南', link: '/note/nginx' },
         { text: 'SSH  配置指南', link: '/note/ssh' },
         { text: 'MacOS 自带VPN链接公司网络', link: '/note/vpn' },
         { text: 'MacOS 邮箱教程', link: '/note/mac-bind-email' },
